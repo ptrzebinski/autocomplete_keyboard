0df3f32c0a6b75f89148e3f252937a3c85dd2e7e
ptrzebinski <patryk.trzebinski@protonmail.com>
2026-03-12 19:00:29 +0100

    feat(main): implement interactive search loop with exit command

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include "../include/AutocompleteEngine.hpp"
+#include <string>
+#include <iostream>
+
+int main() {
+    AutocompleteEngine engine;
+    std::string path = "./data/dictionary.txt";
+    if(!engine.load_dictionary(path)) {
+        return 0;
+    }
+    std::string input;
+    while(true) {
+        std::cout<<"Search >";
+        std::cin >> input;
+        if(input == "exit") {
+            std::cout<<"Closing the program";
+            break;
+        }
+        std::vector<std::string> suggestions = engine.get_suggestions(input);
+        if(suggestions.empty()) {
+            std::cout<<"No answear";
+            continue;
+        }
+        for(const auto& word : suggestions) {
+            std::cout<<word<<" ";
+        }
+        std::cout<<"\n";
+    }
+    return 0;
+}
